e09f275b53260fc71b52266f87cba42aa7d24a36
Justin Clark <151135006+justnspacer@users.noreply.github.com>
2024-04-11 00:04:51 -0500

    adding new project to repo

---
+++ RustyUser.Tests/UserServiceTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RustyUser.Data;
+using RustyUser.Models;
+using RustyUser.Services;
+
+namespace RustyUser.Tests
+{
+    public class UserServiceTests
+    {
+
+    }
+}
