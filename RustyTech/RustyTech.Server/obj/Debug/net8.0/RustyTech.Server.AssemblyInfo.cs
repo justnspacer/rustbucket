ecec727354efa3dcbeafebf483a20f532c8e2896
Justin Clark <151135006+justnspacer@users.noreply.github.com>
2024-04-20 15:11:24 -0500

    updating branch

--- RustyTech/RustyTech.Server/obj/Debug/net8.0/RustyTech.Server.AssemblyInfo.cs
+++ RustyTech/RustyTech.Server/obj/Debug/net8.0/RustyTech.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RustyTech.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457ed82de389a3df2bea3b14f3ec3a00d243ff7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce5bc506438c21c7837737298eedd9beb9e7fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RustyTech.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RustyTech.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
